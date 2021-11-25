1f73564eaea929393155e81445295f821d034d0c
yhjeon92 <farad1173@gmail.com>
2021-11-26 00:39:39 +0900

    Added .gitignore, stop tracking subdirectories .idea and target/

--- static/geoJsonToLeaflet.js
+++ static/geoJsonToLeaflet.js
@@ -1,4 +1,4 @@
-	var map = L.map('map').setView([37.39829480668244, 127.11142604973627], 10);
+	var map = L.map('map').setView([37.45829480668244, 127.11142604973627], 12);
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
@@ -57,7 +57,7 @@
 			return false;
 		},
 
-		onEachFeature: onEachFeature
+		//onEachFeature: onEachFeature
 	}).addTo(map);
 
 	L.geoJSON(trackPoints, {
